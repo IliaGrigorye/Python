5d10ccafda22d01919aeaffb3112f04600cd5101
Ilia Grigoryev <shirokagaricu@mail.ru>
2022-08-10 16:01:21 +0800

    add interface phone book

--- Seminars/Interface/interface.py
+++ Seminars/Interface/interface.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import scrolledtext
-from turtle import width
+
 
 filename = "c:/Programming/programming/Working in Visual Studio Code/Python/Seminars/interface/myphonebook.txt"
 myfile = open(filename, "a+")
@@ -11,64 +11,156 @@ def viewing_contacts():
     myfile = open(filename, "r+") 
     filecontents = myfile.read() 
     if len(filecontents) == 0: 
-        lbl = Label(window, text="В телефонной книге нет контактов", font = ("Arial Bold", 9))
+        lbl = Label(win, text="В телефонной книге нет контактов", font = ("Arial", 9))
         lbl.grid(column=1, row=1)
     else:
-        txt = scrolledtext.ScrolledText(window, width=40, height=10, font = ("Arial Bold", 9))
+        txt = scrolledtext.ScrolledText(win, width=40, height=10, font = ("Arial", 9))
         txt.grid(column=1, row=1)
         txt.insert(INSERT, filecontents)
     myfile.close
 
 def add_contact():
-    lbl = Label(window, text="Имя")  
+    lbl = Label(win, text="Имя", font = ("Arial", 9))  
     lbl.grid(column=1, row=1)
-    txt_one = Entry(window, width=15, font = ("Arial Bold", 9))
-    txt_one.grid(column=2, row=1)
+    txt_one = Entry(win, width=15, font = ("Arial", 9))
+    txt_one.grid(column=2, row=1, stick='wens', padx=5, pady=5)
 
-    lbl = Label(window, text="Фамилия")  
+    lbl = Label(win, text="Фамилия", font = ("Arial", 9))  
     lbl.grid(column=1, row=2)
-    txt_two = Entry(window, width=15, font = ("Arial Bold", 9))
-    txt_two.grid(column=2, row=2)
+    txt_two = Entry(win, width=15, font = ("Arial", 9))
+    txt_two.grid(column=2, row=2, stick='wens', padx=5, pady=5)
 
-    lbl = Label(window, text="Номер телефона")  
+    lbl = Label(win, text="Номер телефона", font = ("Arial", 9))  
     lbl.grid(column=1, row=3)
-    txt_three = Entry(window, width=15, font = ("Arial Bold", 9))
-    txt_three.grid(column=2, row=3)
+    txt_three = Entry(win, width=15, font = ("Arial", 9))
+    txt_three.grid(column=2, row=3, stick='wens', padx=5, pady=5)
 
-    lbl = Label(window, text="Email")  
+    lbl = Label(win, text="Email", font = ("Arial", 9))  
     lbl.grid(column=1, row=4)
-    txt_four = Entry(window, width=15, font = ("Arial Bold", 9))
-    txt_four.grid(column=2, row=4)
-
-    firstname = "{}".format(txt_one.get())
-    lbl.configure(text=firstname)
-    lastname = "{}".format(txt_two.get())
-    lbl.configure(text=lastname) 
-    phoneNum = "{}".format(txt_three.get())
-    lbl.configure(text=phoneNum) 
-    emailID = "{}".format(txt_four.get())
-    lbl.configure(text=emailID) 
+    txt_four = Entry(win, width=15, font = ("Arial", 9))
+    txt_four.grid(column=2, row=4, stick='wens', padx=5, pady=5)
+  
+    def write_txt(data):
+        with open(filename, 'a') as file:
+            file.write(data)
     
-    def click(firstname, lastname, phoneNum, emailID):
-        contactDetails =("[" + firstname + " " + lastname + ", " + phoneNum + ", " + emailID +  "]\n") 
-        myfile = open(filename, "a") 
-        myfile.write(contactDetails)
-        # lbl = Label(window, text="Следующие контактные данные:\n " + contactDetails + "\nбыли успешно сохранены!", font = ("Arial Bold", 14))
-        # lbl.grid(column=2, row=6) 
-        # print( "Следующие контактные данные:\n " + contactDetails + "\nбыли успешно сохранены!")
+    def click():
+        firstname = txt_one.get()
+        lastname = txt_two.get()
+        phoneNum = txt_three.get()
+        emailID = txt_four.get()
 
-    button = Button(window, text='Добавить', font = ("Arial Bold", 9), command=click)    
+        contactDetails =("[" + firstname + " " + lastname + ", " + phoneNum + ", " + emailID +  "]\n") 
+        write_txt(contactDetails)
+        lbl = Label(win, text="Следующие контактные данные:\n " + contactDetails + "\nбыли успешно сохранены!", font = ("Arial Bold", 7))
+        lbl.grid(column=2, row=6) 
+        
+
+    button = Button(win, text='Добавить', font = ("Arial", 9), command=click)    
     button.grid(column=2, row=5)
 
+def searchcontact():
+    lbl = Label(win, text="Введите Имя и Фамилию", font = ("Arial", 9))  
+    lbl.grid(column=1, row=3)
+    txt = Entry(win, width=15, font = ("Arial", 9))
+    txt.grid(column=1, row=4, stick='wens', padx=5, pady=5)
 
-window = Tk()
-window.title("Телефонный справочник")
-window.geometry('500x350')
-lbl = Label(window, text="ГЛАВНОЕ МЕНЮ", font = ("Arial Bold", 14))
-lbl.grid(column=0, row=0)
-btn_one = Button(window, text="Посмотреть все контакты", font = ("Arial Bold", 9), command=viewing_contacts)
-btn_one.grid(column=0, row=1)
-btn_two = Button(window, text="Добавить контакт", font = ("Arial Bold", 9), command=add_contact)
-btn_two.grid(column=0, row=2)
+    def click():
+        searchname = txt.get() 
+        myfile = open(filename, "r+") 
+        filecontents = myfile.readlines()
+        found = False 
+        for line in filecontents: 
+            if searchname in line: 
+                lbl_one = Label(win, text="По ващему запросу найдено:", font = ("Arial", 9))
+                lbl_one.grid(column=1, row=5)
+                lbl_two = Label(win, text=line, font = ("Arial", 9))
+                lbl_two.grid(column=1, row=6)
+                found = True 
+        if found == False: 
+            lbl = Label(win, text="По ващему запросу ни чего не найдено", font = ("Arial", 9))
+            lbl.grid(column=1, row=5)
 
-window.mainloop()
+    button = Button(win, text='Поиск', font = ("Arial", 9), command=click)    
+    button.grid(column=2, row=4)
+
+def delet():
+    lbl = Label(win, text="Введите Имя и Фамилию", font = ("Arial", 9))  
+    lbl.grid(column=1, row=4)
+    txt = Entry(win, width=15, font = ("Arial", 9))
+    txt.grid(column=1, row=5, stick='wens', padx=5, pady=5)
+
+    def click():
+        firstname = txt.get()
+        c = 0
+        k = 0
+
+        with open(filename, "r") as f:
+            lines = f.readlines()
+        with open(filename, "w") as f:
+            for line in lines:
+                if firstname == "":
+                    f.write(line)
+                    c += 1
+                if firstname not in line:
+                    f.write(line)
+                    k += 1
+            
+            if k == len(lines):
+                lbl = Label(win, text="По ващему запросу ни чего не найдено", font = ("Arial", 9))
+                lbl.grid(column=1, row=6)
+            if c > 0:
+                lbl = Label(win, text="По ващему запросу ни чего не найдено", font = ("Arial", 9))
+                lbl.grid(column=1, row=6)
+            else:
+                lbl = Label(win, text=f"Контактный данный {firstname} удалены", font = ("Arial", 9))
+                lbl.grid(column=1, row=6)
+
+    button = Button(win, text='Удалить', font = ("Arial", 9), command=click)    
+    button.grid(column=2, row=5)
+
+win = Tk()
+win.title("Телефонный справочник")
+win.geometry('500x350')
+
+lbl = Label(win, text="Главное меню", font = ("Arial", 12))
+lbl.grid(column=0, row=0, stick='we', padx=5)
+
+btn_one = Button(win, text="Посмотреть все контакты", font = ("Arial", 9), command=viewing_contacts)
+btn_one.grid(column=0, row=1, stick='wens', padx=5, pady=5)
+btn_two = Button(win, text="Добавить контакт", font = ("Arial", 9), command=add_contact)
+btn_two.grid(column=0, row=2, stick='wens', padx=5, pady=5)
+btn_three = Button(win, text="Поиск по контакта", font = ("Arial", 9), command=searchcontact)
+btn_three.grid(column=0, row=3, stick='wens', padx=5, pady=5)
+btn_four = Button(win, text="Удаление контакта", font = ("Arial", 9), command=delet)
+btn_four.grid(column=0, row=4, stick='wens', padx=5, pady=5)
+
+
+
+def stop():
+    btn_one.config(command=donothing)
+    btn_two.config(command=donothing)
+    btn_three.config(command=donothing)
+    btn_four.config(command=donothing)
+
+def donothing():
+    pass
+
+btn_five = Button(win, text="Отмена", font = ("Arial", 9), command=stop)
+btn_five.grid(column=0, row=5, stick='wens', padx=5, pady=5)
+
+
+
+win.grid_columnconfigure(0, minsize=60)
+win.grid_columnconfigure(1, minsize=60)
+win.grid_columnconfigure(2, minsize=60)
+
+win.grid_rowconfigure(0, minsize=30)
+win.grid_rowconfigure(1, minsize=30)
+win.grid_rowconfigure(2, minsize=30)
+win.grid_rowconfigure(3, minsize=30)
+win.grid_rowconfigure(4, minsize=30)
+win.grid_rowconfigure(5, minsize=30)
+win.grid_rowconfigure(6, minsize=30)
+
+win.mainloop()
